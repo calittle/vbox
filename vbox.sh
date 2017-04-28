bca75bfe779d571e8123c29a21956667d378cc9c
C.A. Little <calittle@users.noreply.github.com>
2017-04-28 15:01:30 -0400

    Streamlined dialog handling.

--- vbox.sh
+++ vbox.sh
@@ -16,12 +16,12 @@ function killvm(){
           then
                 dialog --title "Warning" --msgbox "${VMNAME} is not running; cannot shutdown." 5 55
           else
-		dialog --title "Confirm" --yesno "Really power off $VMNAME?" 5 55
+                dialog --title "Confirm" --yesno "Really power off $VMNAME?" 5 55
                 if [ $? = 0 ]
                   then
-			VBoxManage controlvm $VMNAME poweroff
-        		dialog --title "Response" --msgbox "${VMNAME} is powering down." 5 55
-		fi
+                        dialog --title "Response" --infobox "${VMNAME} is powering off." 5 55
+                        VBoxManage controlvm $VMNAME poweroff
+                fi
         fi
 }
 function resetvm(){
@@ -29,12 +29,12 @@ function resetvm(){
           then
                 dialog --title "Warning" --msgbox "${VMNAME} is not running; cannot reset." 5 55
           else
-		dialog --title "Confirm" --yesno "Really reset $VMNAME?" 5 55
+                dialog --title "Confirm" --yesno "Really reset $VMNAME?" 5 55
                 if [ $? = 0 ]
                   then
-		        VBoxManage controlvm $VMNAME reset
-			dialog --title "Response" --msgbox "${VMNAME} is restarting." 5 55
-		fi
+                        dialog --title "Response" --infobox "${VMNAME} is restarting." 5 55
+                        VBoxManage controlvm $VMNAME reset
+                fi
         fi
 }
 function startvm(){
@@ -42,66 +42,69 @@ function startvm(){
           then
                 dialog --title "Warning" --msgbox "${VMNAME} is running; cannot start." 5 55
           else
-	 	nohup VBoxHeadless --startvm $VMNAME & 1>/dev/nul 2>/dev/null
-		dialog --title "Response" --msgbox "${VMNAME} is starting." 5 55
-	fi
+	 	dialog --title "Response" --infobox "${VMNAME} is starting." 5 55
+                nohup VBoxHeadless --startvm $VMNAME >/dev/nul 2>i&1 &
+        fi
 }
 function sleepvm(){
-	if [ $STATE != "running" ]
+        if [ $STATE != "running" ]
           then
-		dialog --title "Warning" --msgbox "${VMNAME} is not running; cannot sleep." 5 55
-	  else
-		dialog --title "Confirm" --yesno "Really hibernate $VMNAME?" 5 55
-		if [ $? = 0 ]
+                dialog --title "Warning" --msgbox "${VMNAME} is not running; cannot sleep." 5 55
+          else
+                dialog --title "Confirm" --yesno "Really hibernate $VMNAME?" 5 55
+                if [ $? = 0 ]
                   then
-                      VBoxManage controlvm $VMNAME savestate
-		fi
-	fi
+                        dialog --title "Response" --infobox "${VMNAME} is hibernating." 5 55
+                        VBoxManage controlvm $VMNAME savestate
+                fi
+        fi
 }
 function mainmenu(){
-	if [ 1$VMNAME = "1" ]
-	 then
-		listvm;
-	fi
+        menuitem=
+        if [ 1$VMNAME = "1" ]
+         then
+                listvm;
+        fi
 
-	#get state of selected VM
-	STATE=$(VBoxManage showvminfo $VMNAME --machinereadable | grep 'VMState=' | cut -d '"' -f2)
+        #get state of selected VM
+        STATE=$(VBoxManage showvminfo $VMNAME --machinereadable | grep 'VMState=' | cut -d '"' -f2)
 
-	dialog --clear  --help-button --backtitle "VirtualBox Command Menu" --title "[ VM Control Panel: Main Menu  ]" --menu "You can use the UP/DOWN arrow keys, the first \n letter of the choice as a hot key, or the \n number keys 1-9 to choose an option.\n\n\
-		Selected VM [${VMNAME}] is ${STATE}.\n\
-		Choose the TASK" 25 55 10 \
-		List "Displays a list of known VMs" \
-		Start "Start a VM (Power on/Restore)" \
-		Kill "Stop a VM (Power off)" \
-		Reset "Reset a VM (Power cycle)" \
-		Quiesce "Hibernate a VM (Sleep)" \
-		Exit "Exit to the shell" 2>"${INPUT}"
+        dialog --clear  --backtitle "VirtualBox Command Menu" --title "[ VM Control Panel: Main Menu  ]" --menu "You can use the UP/DOWN arrow keys, the first \n letter of the choice as a hot key, or the \n number keys 1-9 to choose an option.\n\n\
+                Selected VM [${VMNAME}] is ${STATE}.\n\
+                Choose the TASK" 25 55 10 \
+                List "Displays a list of known VMs" \
+                Start "Start a VM (Power on/Restore)" \
+                Kill "Stop a VM (Power off)" \
+                Reset "Reset a VM (Power cycle)" \
+                Quiesce "Hibernate a VM (Sleep)" \
+                Exit "Exit to the shell" 2>"${INPUT}"
 	
 	menuitem=$(<"${INPUT}")
-	
-	case $menuitem in
-	    List) listvm;;
-	    Start) startvm;;
-	    Kill) killvm;;
-	    Reset) resetvm;;
-	    Quiesce) sleepvm;;
-	    Exit) echo "Bye"; break;;
-	esac
+
+        case $menuitem in
+            List) listvm;;
+            Start) startvm;;
+            Kill) killvm;;
+            Reset) resetvm;;
+            Quiesce) sleepvm;;
+            Exit) break;;
+            *) break;;
+        esac
 }
 #
 # List VM
 #
 function listvm(){
-	#VBoxManage list vms > $OUTPUT
-	VBoxManage list vms | cut -d '{' -f1 > $OUTPUT1
-	VBoxManage list vms | cut -d '"' -f2 > $OUTPUT
-	dialog --clear --backtitle "VirtualBox Command Menu" --title "[ VM Control Panel: List VMs ]" --menu "Select a VM to control" 25 55 10 `paste -d ' ' $OUTPUT $OUTPUT1` 2>"${INPUT}"
-	VMNAME=$(<"${INPUT}")
+        #VBoxManage list vms > $OUTPUT
+        VBoxManage list vms | cut -d '{' -f1 > $OUTPUT1
+        VBoxManage list vms | cut -d '"' -f2 > $OUTPUT
+        dialog --clear --backtitle "VirtualBox Command Menu" --title "[ VM Control Panel: List VMs ]" --menu "Select a VM to control" 25 55 10 `paste -d ' ' $OUTPUT $OUTPUT1` 2>"${INPUT}"
+        VMNAME=$(<"${INPUT}")
 }
 
 while true
 do
-	mainmenu;
+        mainmenu;
 done
 
 # if temp files found, delete em
